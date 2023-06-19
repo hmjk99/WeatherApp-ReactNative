7fd94be7161e311c8f5cff6fdfc3ef17edf3563a
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-06-19 18:53:48 -0400

    general setup

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { View, Text, SafeAreaView, StyleSheet } from "react-native"
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        <Text>Current Weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <Text style={styles.highLow}>High: 8 Low: 8</Text>
       </View>
     </SafeAreaView>
   )
@@ -13,8 +15,26 @@ const App = () => {
 
 
 const styles = StyleSheet.create({
+  wrapper:{
+    flex: 1,
+  },
   container: {
-    backgroundColor: 'pink'
+    backgroundColor: 'pink',
+    flex: 1,
+    alignItems: 'center'
+  },
+  temp:{
+    color: 'black',
+    fontSize: 48,
+  },
+  feels:{
+    fontSize: 30,
+    color: 'black',
+  },
+  highLow:{
+    color: 'black',
+    fontSize: 20
   }
 })
+
 export default App
