7f9e01663226b15ab6446702c2b2f2c1be5ba437
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-06-21 19:17:16 -0400

    refractoring current weather

---
+++ src/components/RowText.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const RowText = (props) => {
+    const { messageOne, messageTwo, containerStyles, messageOneStyles, messageTwoStyles } = styles
+    return (
+        <View style={containerStyles}>
+            <Text style={messageOneStyles}>{messageOne}</Text>
+            <Text style={messageTwoStyles}>{messageTwo}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default RowText
