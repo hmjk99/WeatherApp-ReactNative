0af02704a6450a703dba06b2f4ece78460471277
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-06-21 20:32:04 -0400

    done with styling of nav bar

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
 import React from "react"
-import CurrentWeather from "./src/screens/CurrentWeather"
-import UpcomingWeather from "./src/screens/UpcomingWeather"
-import City from "./src/screens/City"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { Feather } from '@expo/vector-icons';
+import Tabs from "./src/components/Tabs"
 
 const Tab = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        tabBarActiveTintColor: 'blue',
-        tabBarInactiveTintColor: 'grey',
-      }}>
-        <Tab.Screen name={'Current'} component={CurrentWeather} options={{
-          tabBarIcon: ({focused}) => <Feather name={'droplet'} size={25} color={focused ? 'blue' : 'black'} />
-        }} />
-        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} options={{
-          tabBarIcon: ({focused}) => <Feather name={'droplet'} size={25} color={focused ? 'blue' : 'black'} />
-        }} />
-        <Tab.Screen name={'City'} component={City} options={{
-          tabBarIcon: ({focused}) => <Feather name={'droplet'} size={25} color={focused ? 'blue' : 'black'} />
-        }} />
-      </Tab.Navigator>
+      <Tabs/>
     </NavigationContainer>
   )
 }
