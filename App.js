2ff030be67a9eb1463177b1f74debeb99c1bbd44
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-07-03 18:36:37 -0400

    setting expo location

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Tabs from "./src/components/Tabs"
+import * as Location from 'expo-location'
 
 const Tab = createBottomTabNavigator()
 
 const App = () => {
   const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
 
   if (loading) {
     return (
@@ -16,6 +19,19 @@ const App = () => {
       </View>
     )
   }
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositonAsync({})
+      setLocation(location)
+    })
+  }, [])
+
   return (
     <NavigationContainer>
       <Tabs/>
