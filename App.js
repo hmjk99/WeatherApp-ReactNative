1c8a90810acde63a4e466907466d05e2c5c09896
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-07-03 18:29:04 -0400

    added loader

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Tabs from "./src/components/Tabs"
@@ -6,6 +7,15 @@ import Tabs from "./src/components/Tabs"
 const Tab = createBottomTabNavigator()
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tabs/>
@@ -13,4 +23,10 @@ const App = () => {
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  }
+})
 export default App
